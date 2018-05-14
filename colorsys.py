bbbcfc3259c1a3eda3a19c21e1ed515b3c0b001b
John Prah <john.prah@gmail.com>
2018-05-14 16:22:12 -0400

    Returns RGB[0-255]

--- colorsys.py
+++ colorsys.py
@@ -1,6 +1,5 @@
 #This implements a chopped down relevant version of the colorsys library
-#to add HLS and HSV to RGB support
-#Returned values will be a 0-1 float and will need to be returned to 0-255 integer value.
+#to add HLS and HSV to RGB support.  Script altered to return RGB[0-255]
 
 __all__ = ["hls_to_rgb","hsv_to_rgb"]
 
@@ -23,7 +22,7 @@ def hls_to_rgb(h, l, s):
     else:
         m2 = l+s-(l*s)
     m1 = 2.0*l - m2
-    return (_v(m1, m2, h+ONE_THIRD), _v(m1, m2, h), _v(m1, m2, h-ONE_THIRD))
+    return (int(_v(m1, m2, h+ONE_THIRD)*255), int(_v(m1, m2, h)*255), int(_v(m1, m2, h-ONE_THIRD)*255))
 
 def _v(m1, m2, hue):
     hue = hue % 1.0
@@ -51,15 +50,15 @@ def hsv_to_rgb(h, s, v):
     t = v*(1.0 - s*(1.0-f))
     i = i%6
     if i == 0:
-        return v, t, p
+        return int(v * 255), int(t *255), int(p * 255)
     if i == 1:
-        return q, v, p
+        return int(q * 255), int(v *255), int(p * 255)
     if i == 2:
-        return p, v, t
+        return int(p * 255), int(v *255), int(t * 255)
     if i == 3:
-        return p, q, v
+        return int(p * 255), int(q *255), int(v * 255)
     if i == 4:
-        return t, p, v
+        return int(t * 255), int(p *255), int(v * 255)
     if i == 5:
-        return v, p, q
+        return int(v * 255), int(p *255), int(q * 255)
     # Cannot get here
