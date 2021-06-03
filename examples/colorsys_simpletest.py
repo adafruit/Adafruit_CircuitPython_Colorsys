4d2b7f93ae2fc18539efd620c0d6eeb3898c7ceb
GomiHgy <h.furukawa1@gmail.com>
2021-06-03 13:24:44 +0900

    Fix the typo in processing for testing hsv_to_rgb

--- examples/colorsys_simpletest.py
+++ examples/colorsys_simpletest.py
@@ -9,5 +9,5 @@ rgb = colorsys.hls_to_rgb(hls[0], hls[1], hls[2])
 print("HLS {} == RGB {}".format(hls, rgb))
 
 hsv = (0.8, 0.5, 0.5)
-rgb = colorsys.hls_to_rgb(hsv[0], hsv[1], hsv[2])
+rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2])
 print("HSV {} == RGB {}".format(hsv, rgb))
